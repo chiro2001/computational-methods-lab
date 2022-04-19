71217234bcfe503067057d723a46370356f3f2e8
Chiro Liang <Chiro2001@163.com>
2022-04-19 22:53:44 +0800

    change lab report format

---
+++ update_all.cmd
@@ -0,0 +1,10 @@
+del output\*.pdf
+
+python create-pdf.py 实验1
+python create-pdf.py 实验2
+python create-pdf.py 实验3
+python create-pdf.py 实验4
+python create-pdf.py
+
+del pdfs\*.pdf
+copy output\*.pdf pdfs\
